681dc348a918ff83aaf1eebd91e7648d1fdda6bc
jbar-ry <james.barry26@mail.dcu.ie>
2022-02-23 13:59:18 +0000

    update concat script

--- scripts/concat_treebanks.py
+++ scripts/concat_treebanks.py
@@ -43,6 +43,7 @@ def get_ud_treebank_files(dataset_dir: str, treebank_ids: List[str] = None) -> D
         train_path = glob.glob(pathname).pop()
         treebank_path = os.path.dirname(train_path)
         treebank = os.path.basename(treebank_path)
+        datasets[treebank] = {}
 
         train_file = [file for file in os.listdir(treebank_path) if file.endswith("train.conllu")]
         dev_file = [file for file in os.listdir(treebank_path) if file.endswith("dev.conllu")]
@@ -52,11 +53,15 @@ def get_ud_treebank_files(dataset_dir: str, treebank_ids: List[str] = None) -> D
         dev_file = os.path.join(treebank_path, dev_file[0]) if dev_file else None
         test_file = os.path.join(treebank_path, test_file[0]) if test_file else None
 
-        datasets[treebank] = (train_file, dev_file, test_file)
+        datasets[treebank]["train"] = train_file
+        datasets[treebank]["dev"] = dev_file
+        datasets[treebank]["test"] = test_file
 
     return datasets
 
+
 datasets = get_ud_treebank_files(args.dataset_dir, args.treebank_ids)
+
 all_treebank_files = [datasets[t] for t in datasets.keys()]
 all_treebanks = "+".join(t for t in datasets.keys())
 
@@ -65,12 +70,14 @@ if not os.path.exists(output_path):
     print(f"Creating output path {output_path}")
     os.makedirs(output_path)
 
-basenames = []
+basenames = [] # a None here will get rid of dev
 tbids = [] # use a list to maintain order of tbids
 merged_basenames = []
 
+# for treebank, files in datasets.items():
+
 for treebank, files in datasets.items():
-    for file_path in files:
+    for file_path in files.values():
         try:
             basename = os.path.basename(file_path)
             tbid = basename.split("-")[0]
@@ -88,13 +95,26 @@ for b in basenames:
     if merged not in merged_basenames:
         merged_basenames.append(merged)
 
-train, dev, test = list(zip(*[datasets[k] for k in datasets.keys()]))
-
-for file_group, name in zip([train, dev, test], merged_basenames):
-    print(f"{file_group} == {name}")
-    with open(os.path.join(output_path, name), 'w') as write:
+for key in ["train", "dev", "test"]:
+    file_group = []
+    # collect all treebank files for the given split
+    for treebank in datasets.keys():
+        _file = datasets[treebank][key]
+        file_group.append(_file)
+    for i, b in enumerate(merged_basenames):
+        if key in b:
+            name = merged_basenames[i]
+    
+    # write
+    assert key in name # just to be sure we're not mixing file types
+    file_output_path = os.path.join(output_path, name)
+    print()
+    print(f"Writing file group {key} containing files: {file_group} to {file_output_path}")
+    with open(file_output_path, 'w') as write:
         for _file in file_group:
             if not _file:
                 continue
             with open(_file, 'r') as read:
                 shutil.copyfileobj(read, write)
+
+print("done")
