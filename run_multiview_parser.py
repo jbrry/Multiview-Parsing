668363de78f71abbbebcf58b430bec9bd2f207ed
jbar-ry <james.barry26@mail.dcu.ie>
2022-05-30 16:28:56 +0100

    update run script

--- run_multiview_parser.py
+++ run_multiview_parser.py
@@ -23,7 +23,7 @@ parser.add_argument(
 )
 parser.add_argument(
     "--dataset-dir",
-    default="data/ud-treebanks-v2.8",
+    default="data/ud-treebanks-v2.8-sampled",
     type=str,
     help="The path containing all UD treebanks",
 )
@@ -62,6 +62,12 @@ parser.add_argument(
     help="Whether to do lower case or not"
 )
 parser.add_argument(
+    "--recover",
+    default=False,
+    action="store_true",
+    help="Flag to recover training"
+)
+parser.add_argument(
     "--use-cross-stitch",
     default=False,
     action="store_true",
@@ -79,6 +85,24 @@ parser.add_argument(
     help="Metadata to inclde to experiment run.",
 )
 parser.add_argument(
+    "--desired-batch-size",
+    type=int,
+    default=32,
+    help="Batch size for experiment, will be used to calculate steps for grad accum.",
+)
+parser.add_argument(
+    "--gpu-batch-size",
+    type=int,
+    default=8,
+    help="Batch size on a physical device, hardware-bound.",
+)
+parser.add_argument(
+    "--num-gpus",
+    type=int,
+    default=1,
+    help="Number of GPUs.",
+)
+parser.add_argument(
     "--random-seed",
     type=str,
     default="13337",
@@ -88,6 +112,7 @@ parser.add_argument(
     "--fields",
     default=[
         "dataset_reader",
+        "data_loader",
         "model",
         "heads",
         "train_data_path",
@@ -95,6 +120,7 @@ parser.add_argument(
         "test_data_path",
         "validation_metric",
         "random_seed",
+        "trainer",
         "numpy_seed",
         "pytorch_seed",
     ],
@@ -125,7 +151,12 @@ if args.metadata:
 else:
     metadata_string = ""
 
-run_name = original_json_file[0] + f"-{model_identifier}-" + "+".join(args.tbids) + metadata_string + "-" + args.random_seed
+if args.use_cross_stitch:
+    cross_stitch_string = "-cross-stitch"
+else:
+    cross_stitch_string = ""
+
+run_name = original_json_file[0] + f"-{model_identifier}-" + "+".join(args.tbids) + metadata_string + cross_stitch_string + "-" + args.random_seed
 config_name = run_name + original_json_file[1]
 
 logdir = f"logs/dependency_parsing_multiview/{run_name}"
@@ -146,11 +177,16 @@ class ExperimentBuilder:
         self.model_type = args.model_type
         self.head_type = args.head_type
         self.fields = args.fields
+        self.recover = args.recover
+        self.use_cross_stitch = args.use_cross_stitch
         self.pretrained_model_name = args.pretrained_model_name
         self.dataset_dir = args.dataset_dir
         self.dataset_dir_concatenated = args.dataset_dir_concatenated
         self.random_seed = args.random_seed
         self.results_dir = args.results_dir
+        self.desired_batch_size =  args.desired_batch_size
+        self.gpu_batch_size = args.gpu_batch_size
+        self.num_gpus = args.num_gpus
 
         self.GDRIVE_DEST="gdrive:Parsing-PhD-Folder/UD-Data/cross-lingual-parsing/Multilingual_Parsing_Meta_Structure/experiment_logs/"
 
@@ -214,10 +250,12 @@ class ExperimentBuilder:
                     tmp_heads[f"{tbid}_{args.head_type}"] = placeholder_value
                     data["model"]["heads"].update(tmp_heads)
 
-                    # delete placeholder
+                    # delete placeholder and check meta head
                     if i == nb_tbids:
                         del data["model"]["heads"][TBID_PLACEHOLDER]
-
+                        if self.model_type == "multiview" and self.use_cross_stitch:
+                            data["model"]["heads"]["meta_dependencies"]["use_cross_stitch"] = bool(True)
+                           
                 elif k == "train_data_path":
                     _file = f"{tbid}-ud-train.conllu"
                     # we will just get the main paths once
@@ -261,13 +299,27 @@ class ExperimentBuilder:
                         del data["test_data_path"][TBID_PLACEHOLDER]
                         if not data["test_data_path"]:
                             del data["test_data_path"]
-                    
+                
+                elif k == "data_loader":
+                    if i == nb_tbids:
+                        data["data_loader"]["scheduler"]["batch_size"] = self.gpu_batch_size
+
+                elif k == "trainer":
+                    if i == nb_tbids:
+                        effective_batch_size = self.gpu_batch_size * self.num_gpus
+                        num_gradient_accumulation_steps = self.desired_batch_size / effective_batch_size
+                        data["trainer"]["num_gradient_accumulation_steps"] = int(num_gradient_accumulation_steps)
+
                 elif k == "validation_metric":
-                    if self.model_type == "singleview" or "singleview-concat":
+                    if self.model_type == "singleview": # or "singleview-concat":
+                        self.metric = f"+{tbid}_dependencies_LAS"
+                    elif self.model_type == "singleview-concat":
                         self.metric = f"+{tbid}_dependencies_LAS"
                     elif self.model_type == "multiview":
                         self.metric = f"+meta_dependencies_LAS"
-                    data["trainer"]["validation_metric"] = self.metric
+                    
+                    if i == nb_tbids:
+                        data["trainer"]["validation_metric"] = self.metric
 
                 elif k == "random_seed":
                     data["random_seed"] = self.random_seed
@@ -281,10 +333,11 @@ class ExperimentBuilder:
             json.dump(data, fo, indent=2)
             print(json.dumps(data, indent=2))
 
-        # check if the model is on google drive, if so download it and unpack it for prediction
-        cmd = f"rclone copy {self.GDRIVE_DEST}{run_name}.tar.gz {os.path.dirname(logdir)}"
-        rcmd = subprocess.call(cmd, shell=True)
-        
+        # if no tar file and no logdir, then try download it.
+        if not os.path.exists(f"{logdir}.tar.gz") and not os.path.exists(f"{logdir}"):
+            cmd = f"rclone copy {self.GDRIVE_DEST}{run_name}.tar.gz {os.path.dirname(logdir)}"
+            rcmd = subprocess.call(cmd, shell=True)
+
         # if we downloaded a file, extract it
         if os.path.exists(f"{logdir}.tar.gz"):
             cmd = f"tar -xvzf {logdir}.tar.gz"
@@ -294,13 +347,37 @@ class ExperimentBuilder:
         if os.path.isfile(f"{logdir}/model.tar.gz"):
             print(f"skipping training, file exists.")
         else:
-            # remove tarred file which only contains stdout etc.
-            print("removing tarred file with no model")
-            cmd = f"rm {logdir}.tar.gz"
-            rcmd = subprocess.call(cmd, shell=True)
+            if not self.recover:
+                # remove tarred file which only contains stdout etc.
+                print("removing tarred file with no model")
+                cmd = f"rm {logdir}.tar.gz"
+                rcmd = subprocess.call(cmd, shell=True)
+                
+                print("training from scratch")
+                cmd = f"allennlp train -f {dest_file} -s {logdir} --include-package multiview_parser"
+            else:
+                # if we're recovering, we need to make sure there is a valid model state/training state to recover from.
+                # the next bit of code searches for model states and checks if there is a corresponding training state.
+                # if not, the model state will be deleted, falling back to an earlier state we can train from.
+                model_files = []
+                for f in os.listdir(f"{logdir}"):
+                    # if we have a model state, check if we also have a training state. If not delete it
+                    if "model_state" in f:
+                        model_files.append(f)
 
-            print("training from scratch")
-            cmd = f"allennlp train -f {dest_file} -s {logdir} --include-package multiview_parser"
+                for mf in sorted(model_files): # sort on epoch number
+                    epoch_item = mf.split("_")[2] # ['model', 'state', 'e46', 'b0.th']
+                    training_state = f"training_state_{epoch_item}_b0.th"
+                    # if there's a training state, do nothing
+                    if os.path.exists(f"{logdir}/{training_state}"):
+                        continue
+                    else: # else, we need to delete the model state
+                        print(f"removing model file with no training state: {logdir}/{mf}")
+                        os.remove(f"{logdir}/{mf}")
+                
+                print("recovering training")
+                cmd = f"allennlp train -s {logdir} -r --include-package multiview_parser {dest_file}"
+
             print("\nLaunching training script!")
             rcmd = subprocess.call(cmd, shell=True)
 
@@ -309,12 +386,12 @@ class ExperimentBuilder:
             self.predict_singleview()
         elif self.model_type == "singleview-concat":
             self.predict_singleview_concat()
-            raise ValueError()
-        # elif self.model_type == "multiview":
-        #     self.predict_multiview()
+        elif self.model_type == "multiview":
+            self.predict_multiview()
 
-        # tar the file (in the case we've just trained a new model)
-        if not os.path.exists(f"{logdir}.tar.gz"):
+
+        # tar the file (in the case we've just trained a new model) or if we recovered and need to create a new one.
+        if not os.path.exists(f"{logdir}.tar.gz") or self.recover:
             cmd = f"tar -cvzf {logdir}.tar.gz {logdir}/"
             print(f"tarring {tbid}")
             rcmd = subprocess.call(cmd, shell=True)
@@ -332,7 +409,6 @@ class ExperimentBuilder:
     def predict_singleview(self):
         """Returns the command to run the predictor with the appropriate inputs/outputs."""
         print("predicting singleview")
-
         for tbid in args.tbids:
             predictor_args = '{"head_name": ""}'
             predictor_json = json.loads(predictor_args)
@@ -429,37 +505,57 @@ class ExperimentBuilder:
                         rcmd = subprocess.call(cmd, shell=True)
 
     def predict_multiview(self):
-        """Returns the command to run the predictor with the appropriate inputs/outputs."""
+        print("predicting multiview")
+        # predicter args stay outside the loop as we are always using the meta head.
+        predictor_args = '{"head_name": ""}'
+        predictor_json = json.loads(predictor_args)
+        predictor_json["head_name"] = "meta_dependencies"
+        predictor_json_string = json.dumps(predictor_json)
+        
         for tbid in args.tbids:
-            predictor_args = '{"head_name": ""}'
-            predictor_json = json.loads(predictor_args)
-            predictor_json["head_name"] = f"meta_dependencies"
-            predictor_json_string = json.dumps(predictor_json)
-            
-            outfile = f"{results_dir}/{run_name}-{tbid}-ud-dev.conllu"
-            result_evalfile = f"{results_dir}/{run_name}-{tbid}-ud-dev-eval.txt"
+            target_files = []
+            # Need to try here as the value will be present from obtaining the data from the previous tbid.
+            # In the singleview case it should work fine there because there is just one iteration.
+            try:
+                target_file = data["validation_data_path"][tbid]
+                target_files.append(target_file)
+            except KeyError:
+                print("no dev data found")
 
-            target_file = data["validation_data_path"][tbid]
+            try:
+                target_file = data["test_data_path"][tbid]
+                target_files.append(target_file)
+            except KeyError:
+                print("no test data found")
 
-            cmd = f"allennlp predict {logdir}/model.tar.gz {target_file} \
-                --output-file {outfile} \
-                --predictor conllu-multitask-predictor \
-                --include-package multiview_parser \
-                --use-dataset-reader \
-                --predictor-args '{predictor_json_string}' \
-                --multitask-head {tbid} \
-                --batch-size 32 \
-                --cuda-device 0 \
-                --silent"
+            for target_file in target_files:
+                print(target_file)
+                filename_short = os.path.splitext(os.path.basename(target_file))[0]
+                if "dev" in filename_short:
+                    mode = "dev"
+                elif "test" in filename_short:
+                    mode = "test"
 
-            print(f"predicting {tbid}")
-            rcmd = subprocess.call(cmd, shell=True)
-            cmd = f"python scripts/conll18_ud_eval.py -v {target_file} {outfile} > {result_evalfile}"
-            print(f"evaluating {tbid}")
-            rcmd = subprocess.call(cmd, shell=True)
+                outfile = f"{self.results_dir}/{run_name}-{tbid}-ud-{mode}.conllu"
+                result_evalfile = f"{self.results_dir}/{run_name}-{tbid}-ud-{mode}-eval.txt"
+                
+                cmd = f"allennlp predict {logdir}/model.tar.gz {target_file} \
+                    --output-file {outfile} \
+                    --predictor conllu-multitask-predictor \
+                    --include-package multiview_parser \
+                    --use-dataset-reader \
+                    --predictor-args '{predictor_json_string}' \
+                    --multitask-head {tbid} \
+                    --batch-size 32 \
+                    --cuda-device 0 \
+                    --silent"
+
+                print(f"predicting {tbid} in mode {mode}")
+                rcmd = subprocess.call(cmd, shell=True)
+                cmd = f"python scripts/conll18_ud_eval.py -v {target_file} {outfile} > {result_evalfile}"
+                print(f"evaluating {tbid}")
+                rcmd = subprocess.call(cmd, shell=True)
 
 
 experiment_builder = ExperimentBuilder(args)
 experiment = experiment_builder.prepare_and_run_experiment()
-
-
